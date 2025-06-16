d70dcb496a10886ce38d852384d8fa65d41ed379
Krivoshapkin Eduard <buttersus@mail.ru>
2025-06-16 16:30:20 +0900

    refactor: change standard template

--- modules/template/config.nix
+++ modules/template/config.nix
@@ -1,68 +1,26 @@
-# Template module - Implementation
-{ config, lib, pkgs, ... }:
+#############################
+# DELETE THIS COMMENT BLOCK #
+#############################
+# This file contains:       #
+# - Home Manager packages   #
+# - Home Manager options    #
+# - NixOS options           #
+#############################
+
+{ config, lib, pkgs, isNixOS, ... }:
 
 let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf recursiveUpdate;
   cfg = config.modules.template;
 in {
-  config = mkIf cfg.enable {
-    # ===== System-level configuration =====
-    
-    # Add system packages if needed
-    environment.systemPackages = with pkgs; [
-      cfg.package
-    ] ++ cfg.extraPackages;
-    
-    # Example service configuration
-    services.someService = {
-      enable = true;
-      # Other service settings
-    };
-    
-    # Example program configuration
-    programs.someProgram = {
-      enable = true;
-      # Other program settings
-    };
-    
-    # Example of conditional configuration
-    systemd.services.conditionalService = lib.mkIf cfg.someBoolOption {
-      description = "Example Conditional Service";
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        ExecStart = "${cfg.package}/bin/some-binary";
-        Restart = "on-failure";
-      };
-    };
-    
-    # ===== User-level (Home Manager) configuration =====
-    
-    # Primary user configuration through home-manager
-    home-manager.users.${config.primaryUser} = { pkgs, ... }: {
-      # Add user packages if needed
+  config = recursiveUpdate mkIf cfg.enable {
+    home-manager.users.${config.primaryUser} = {
       home.packages = with pkgs; [
-        # User-specific packages
+        someNixOSPackage
       ];
-      
-      # Configure user programs
-      programs.someProgram = {
-        enable = true;
-        settings = {
-          # Use values from the module configuration
-          setting1 = cfg.settings.nestedOption1;
-          setting2 = cfg.settings.nestedOption2;
-        };
-      };
-      
-      # Create user config files
-      xdg.configFile = {
-        "someapp/config.json".text = ''
-          {
-            "setting": "${cfg.someStringOption}",
-            "value": ${toString cfg.someIntOption}
-          }
-        '';
-      };
     };
+  } mkIf cfg.enable && isNixOS {
+    services.someService.someOption = true;
   };
 }
+    
