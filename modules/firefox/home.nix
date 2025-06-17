53770f31f7de8314934e066d7306e4383c40b741
Krivoshapkin Eduard <buttersus@mail.ru>
2025-06-17 20:26:22 +0900

    feat: customize firefox UI with compact theme and minimal toolbar layout

--- modules/firefox/home.nix
+++ modules/firefox/home.nix
@@ -13,10 +13,73 @@ let
     programs.firefox = {
       enable = true;
       languagePacks = [ "en-US" "ru" ];
+      # User specific settings
       profiles.${config.primaryUser} = {
         settings = {
           "browser.tabs.closeWindowWithLastTab" = false;
+          # Enable userChrome.css
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          # Enable compact mode in UI
+          "browser.compactmode.show" = true;
+          # Set compact density
+          "browser.uidensity" = 1;
+          # Hide bookmarks in toolbar
+          "browser.toolbars.bookmarks.visibility" = "never";
+          "browser.bookmarks.restore_default_bookmarks" = false; # Corrected typo
+          # Don't show warn
+          "browser.aboutConfig.showWarning" = false;
+          # This JSON string defines the placement of all toolbar items.
+          # We place only the urlbar and extensions button on the nav-bar.
+          # Other items are moved to the overflow menu.
+          "browser.uiCustomization.state" = ''
+            {
+              "placements": {
+                "widget-overflow-fixed-list": [
+                  "downloads-button",
+                  "history-panelmenu",
+                  "find-button",
+                  "fxa-toolbar-menu-button",
+                  "firefox-view-button",
+                  "back-button",
+                  "forward-button",
+                  "stop-reload-button",
+                  "home-button"
+                ],
+                "nav-bar": [
+                  "urlbar-container",
+                  "unified-extensions-button"
+                ],
+                "toolbar-menubar": ["menubar-items"],
+                "TabsToolbar": [
+                  "tabbrowser-tabs",
+                  "new-tab-button",
+                  "alltabs-button"
+                ],
+                "PersonalToolbar": []
+              },
+              "seen": [
+                "save-to-pocket-button",
+                "developer-button",
+                "unified-extensions-button"
+              ],
+              "dirtyAreaCache": [
+                "nav-bar",
+                "toolbar-menubar",
+                "TabsToolbar",
+                "PersonalToolbar",
+                "widget-overflow-fixed-list"
+              ],
+              "currentVersion": 22,
+              "newElementCount": 0
+            }
+          '';
+          # Hide sidebar
+          "sidevar.visibility" = "hide-sidebar";
+          # Don't trigger menu bar on <Alt> key press
+          "ui.key.menuAccessKeyFocuses" = false;
         };
+        # Apply one-line firefox theme
+        userChrome = builtins.readFile ./userChrome.css;
       };
       policies = {
         DisableTelemetry = true;
