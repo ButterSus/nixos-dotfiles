6571e8bab27477a3f65eb92d0aaba1582063581c
Krivoshapkin Eduard <buttersus@mail.ru>
2025-06-25 13:59:30 +0900

    chore: add comments about jetbrains plugins

--- modules/intellij-community/nixos.nix
+++ modules/intellij-community/nixos.nix
@@ -12,7 +12,12 @@ in {
   config = mkIf cfg.enable {
     # System Packages
     environment.systemPackages = with pkgs; [
-      jetbrains.idea-community-bin  # Source will take forever to build, use binary please!
+      # Patched plugins can be found here: https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/editors/jetbrains/plugins/plugins.json
+
+      # It seems jetbrains.plugins.addPlugins is currently unstable,
+      # so we are not able to use it properly.
+      # Recommended approach: Use builtin idea-community sync
+      jetbrains.idea-community
     ];
   };
 }
