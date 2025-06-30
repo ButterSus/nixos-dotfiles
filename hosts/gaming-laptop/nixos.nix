02bb7938b448b217f6ff650a9f7ffe77191ae92f
Krivoshapkin Eduard <buttersus@mail.ru>
2025-06-30 15:20:28 +0900

    feat: add /media/recordings

--- hosts/gaming-laptop/nixos.nix
+++ hosts/gaming-laptop/nixos.nix
@@ -7,6 +7,27 @@
   ];
 
   config = {
-    # ...
+    # My internal partition
+    fileSystems."/media/recordings" = {
+      device = "/dev/disk/by-uuid/2eb3790e-d83f-4ecc-9e4a-675a88b9d5c5";
+      fsType = "ext4";
+      options = [ # If you don't have this options attribute, it'll default to "defaults" 
+        # boot options for fstab. Search up fstab mount options you can use
+        "users" # Allows any user to mount and unmount
+        "nofail" # Prevent system from failing if this drive doesn't mount
+      ];
+    };
+
+    # Allow user to write to internal partition
+    systemd.services.fix-recordings-ownership = {
+      description = "Set ownership of recordings directory";
+      after = [ "media-recordings.mount" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.coreutils}/bin/chown ${config.primaryUser}:users /media/recordings";
+        RemainAfterExit = true;
+      };
+    };
   };
 }
